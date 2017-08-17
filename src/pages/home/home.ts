d966610980bef6c9cc7d4637dad1c87283a85b3a
akash-gupta-146 <ag.akash007@gmail.com>
2017-08-18 01:15:15 +0530

    Updated home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,12 +3,19 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  styles: [``
+  ]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private nav : NavController) {
 
+  }
+    openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page);
   }
 
 }
