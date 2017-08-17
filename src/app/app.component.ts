8620140a9289657c5322027c600506b05eaaf986
akash-gupta-146 <ag.akash007@gmail.com>
2017-08-18 01:14:29 +0530

    Updated side bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AboutUsPage } from '../pages/aboutus/aboutus';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +25,21 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      // { title: 'List', component: ListPage },
+      { title: 'Login', component: LoginPage },
+      { title: 'Register', component: RegisterPage },
+      { title: 'About us', component: AboutUsPage },
+
+      { title: 'Trending Businesses', component: AboutUsPage },
+      { title: 'Dynamic Count', component: AboutUsPage },
+      { title: 'Testimonials', component: AboutUsPage },
+      { title: 'Cancellations', component: AboutUsPage },
+      { title: 'How we are different', component: AboutUsPage },
+      { title: 'Contact us', component: AboutUsPage },
+      { title: 'What we do', component: AboutUsPage },
+      { title: 'How it works', component: AboutUsPage },
+      { title: 'Terms and Conditions', component: AboutUsPage },
+
     ];
 
   }
